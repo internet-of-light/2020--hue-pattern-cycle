aca6975b41d0382f7a7b6c9f1da6765759d39b25
Ross Monroe <rossmonroe@me.com>
2020-02-25 17:04:54 -0800

    updated patterns for lower and upper lights

--- hue-master-cycle/hue-master-cycle.ino
+++ hue-master-cycle/hue-master-cycle.ino
@@ -11,9 +11,8 @@ String ip = "172.28.219.179"; // Sieg Master IP
 String api_token = "rARKEpLebwXuW01cNVvQbnDEkd2bd56Nj-hpTETB"; // Sieg Master API Token
 
 //Sieg lower floor
-int lowerLights[] = {10, 23, 11, 15, 7, 14, 22, 21, 16};
-int upperLights[] = {12, 5, 13, 17, 20, 26, 19, 9, 18, 25, 8, 24};
-
+int lowerLights[] = {10, 57, 11, 15, 7, 14, 22, 21, 16};
+int upperLights[] = {18, 20, 12, 25, 26, 5, 8, 19, 13, 24, 9, 17};
 
 //light settings
 int transitionTime = 5;
@@ -27,9 +26,9 @@ typedef struct {
 
 randomValues rv;
 
-unsigned long currentTime, lastPattern, lightLastUpdate; //timing
+unsigned long currentTime, lastPattern, timerOne, timerTwo; //timing
 
-bool debug = false;
+bool debug = true;
 
 void setup() {
   Serial.begin(115200);
@@ -39,8 +38,15 @@ void setup() {
 void loop() {
   currentTime = millis(); //Update time 
   iterateLights();
-  if (currentTime - lightLastUpdate > 20000) {
+  if (currentTime - timerOne > 60000) {
     lowerLightsPattern();
+  }
+  if (currentTime - timerTwo > 180000) {
+    for( int i = 0; i < 3; i++){
+      upperLightsPatternWave();
+    }
+    delay(5000);
+    timerTwo = currentTime;
   }
   if (WiFi.status() != WL_CONNECTED) {
       delay(1);
@@ -58,15 +64,13 @@ void iterateLights(){
 
 void lowerLightsCycle(){
       randomLightValues();
-      int light = random(0,9);
-      int lightNum = lowerLights[light];
+      int lightNum = lowerLights[random(0,9)];
       changeLight(lightNum, transitionTime, "on", "true", "bri", rv.bri, "hue", rv.col, "sat", rv.sat);
 }
 
 void upperLightsCycle(){
       randomLightValues();
-      int light = random(0,12);
-      int lightNum = upperLights[light];
+      int lightNum = upperLights[random(0,12)];
       changeLight(lightNum, transitionTime, "on", "true", "bri", rv.bri, "hue", rv.col, "sat", rv.sat);
 }
 
@@ -78,12 +82,12 @@ void lowerLightsPattern(){
       delay(400);
       changeLight(21, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
       changeLight(7, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
-      changeLight(23, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
+      changeLight(57, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
       delay(400);
       changeLight(16, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
       changeLight(14, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
       changeLight(11, patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
-      lightLastUpdate = currentTime;
+      timerOne = currentTime;
       delay(5000);
       for (int i = 0; i < 9; i++){
         changeLight(lowerLights[i], 0, "on", "false", "bri", rv.bri, "hue", rv.col, "sat", "254");
@@ -94,10 +98,27 @@ void lowerLightsPattern(){
       delay(1000);
 }
 
+void upperLightsPatternWave(){
+      randomLightValues();
+      for(int i = 0; i < 12; i++) {
+        changeLight(upperLights[i], patternTransition, "on", "true", "bri", "50", "hue", rv.col, "sat", "254");
+        changeLight(upperLights[i], patternTransition, "on", "true", "bri", "254", "hue", rv.col, "sat", "254");
+      }
+      delay(1000);
+}
+
 void randomLightValues(){
-  rv.col = String(random(30000, 60000));
+  rv.col = String(random(38000, 60000));
   rv.bri = String(random(190, 254));
   rv.sat = String(random(180, 254));
+  if (debug){
+    Serial.println("NEW VALUES");
+    Serial.println("NEW VALUES");
+    Serial.println("NEW VALUES");
+    Serial.println("NEW VALUES");
+    Serial.println("NEW VALUES");
+    Serial.println("NEW VALUES");
+  }
 }
 
 void setup_wifi() {
